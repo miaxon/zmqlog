5d0a758d1a66d74c5b9c1acae823413562e51293
conalex <alexcon314@gmail.com>
2017-04-18 00:30:40 +0300

    initial commit

--- lib/macrodef.h
+++ lib/macrodef.h
@@ -16,7 +16,7 @@
 
 #define o(x) std::cout << x << std::endl
 
-#define ZMQLOG_IPC "/tmp/zmqlog.ipc"
+#define ZMQLOG_IPC "ipc:///tmp/zmqlog.ipc"
 
 #define LOG_INTERNAL_ST(x, y) auto x = y.logger_st()
 #define LOG_INTERNAL_MT(x, y) auto x = y.logger_mt()
